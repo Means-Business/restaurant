8f18f359a0bf4553e7ca3e07ed12ab36ca209c4e
ManotLuijiu <manot_luijiu@hotmail.com>
2019-03-29 20:07:30 +0700

    adding logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,17 +18,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `gatsby-starter-restaurant`,
+        short_name: `restaurant`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.svg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
   ],
-}
+};
